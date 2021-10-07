6eb227fff00fa6e42777b5a88ecb376f97f53d7d
bniess <bniess@sevanova.com>
2021-10-07 18:27:07 +0200

    Disabled the home page

---
+++ tests/Feature/HomePageTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class HomePageTest extends TestCase
+{
+
+    public function test_homepage_is_disabled()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(400);
+        $response->assertSeeText('API USAGE ONLY');
+    }
+}
