c06062cf5fca935218e372befd5a8b0f5170c0b0
bniess <bniess@sevanova.com>
2021-10-12 18:15:02 +0200

    Started a seeder to generate boards and users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,16 @@
 
 namespace Database\Seeders;
 
+
+use App\Models\Board;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
+
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        Board::factory()->count(10)->for(User::factory())->create();
     }
 }
