1bcf8a6156ef21129056051ea0062b59a47682e6
Benjamin Niess <benjamin.niess@gmail.com>
2021-10-20 10:58:17 +0200

    Moved numbers actions to a NumberController class

--- app/Policies/BoardPolicy.php
+++ app/Policies/BoardPolicy.php
@@ -10,14 +10,6 @@ class BoardPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Board  $board
-     *
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function update(User $user, Board $board)
     {
         return $user->id === $board->user_id;
