c84c3c7c91cb8ab61b15a4a52f0e82f88bd283a2
benjaminniess <benjamin.niess@gmail.com>
2021-11-02 12:14:02 +0100

    Removed the NumberRepository abstraction inteface because param changes between models

--- app/Contracts/BoardRepositoryInterface.php
+++ app/Contracts/BoardRepositoryInterface.php
@@ -2,7 +2,18 @@
 
 namespace App\Contracts;
 
-interface BoardRepositoryInterface extends RepositoryInterface
-{
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
+interface BoardRepositoryInterface
+{
+    public function all(): Collection;
+
+    public function findByID(int $modelId): Model;
+
+    public function deleteById(int $modelId): void;
+
+    public function store(array $modelProperties): Model;
+
+    public function update(int $modelId, array $modelProperties): Model;
 }
--- app/Contracts/NumberRepositoryInterface.php
+++ app/Contracts/NumberRepositoryInterface.php
@@ -2,8 +2,19 @@
 
 namespace App\Contracts;
 
+use App\Models\Board;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
-interface NumberRepositoryInterface extends RepositoryInterface
+interface NumberRepositoryInterface
 {
+    public function all(): Collection;
 
+    public function findByID(int $modelId): Model;
+
+    public function deleteById(int $modelId): void;
+
+    public function store(Board $board, array $modelProperties): Model;
+
+    public function update(int $modelId, array $modelProperties): Model;
 }
