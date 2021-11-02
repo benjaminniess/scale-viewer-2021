9171515833b392811f8c3cf7e73ef95be6e969da
benjaminniess <benjamin.niess@gmail.com>
2021-11-02 11:34:16 +0100

    Created a common Repository Interface to prepare numbers repository interface

--- app/Contracts/BoardRepositoryInterface.php
+++ app/Contracts/BoardRepositoryInterface.php
@@ -2,18 +2,7 @@
 
 namespace App\Contracts;
 
-use App\Models\Board;
-use Illuminate\Support\Collection;
-
-interface BoardRepositoryInterface
+interface BoardRepositoryInterface extends RepositoryInterface
 {
-    public function all(): Collection;
 
-    public function findByID(int $boardId): Board;
-
-    public function deleteById(int $boardId): void;
-
-    public function store(array $boardProperties): Board;
-
-    public function update(int $boardId, array $boardProperties): Board;
 }
